a9dc56eeaa6eb90d7c73ecb24e446f7407019eb7
cepexa <cepexa7@yandex.ru>
2021-03-22 23:15:59 +0300

    HW_tree

--- DataContainers/BinaryTree/main.cpp
+++ DataContainers/BinaryTree/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
 class Tree {
+
+	class Iterator;
 	struct Element {
-		int data;
+		T data;
 		Element* pLeft;
 		Element* pRight;
-		Element(int data, Element* pLeft = nullptr, Element* pRight = nullptr) :
+		Element(T data, Element* pLeft = nullptr, Element* pRight = nullptr) :
 			data(data), pLeft(pLeft), pRight(pRight){}
 	}*root;
-	void insert(int data, Element* root) {
+	void insert(T data, Element* root) {
 		if (this->root) {
 			if (root) {
 				if (data < root->data) {
@@ -35,15 +38,16 @@ class Tree {
 			delete root;
 		}
 	}
-	int minValue(Element* root) {
+	T minValue(Element* root) {
 		return (root->pLeft) ? minValue(root->pLeft): root->data;
 	}
-	int maxValue(Element* root) {
+	T maxValue(Element* root) {
 		return (root->pRight) ? maxValue(root->pRight) : root->data;
 	}
 	int size(Element* root) {
 		return (root) ? size(root->pLeft) + size(root->pRight) + 1 : 0;
 	}
+	//sum только для числовых типов оставим int
 	int sum(Element* root) {
 		return (root) ?  sum(root->pLeft) + sum(root->pRight) + root->data : 0;
 	}
@@ -56,6 +60,61 @@ class Tree {
 			copy(root->pRight);
 		}
 	}
+	Iterator find(T data, Iterator cur) {
+		if (cur) {
+			Element* temp = cur;
+			return (data < *cur) ? find(data, temp->pLeft) : (data > *cur) ? find(data, temp->pRight) : cur;
+		}
+		else {
+			return cur;
+		}
+	}
+	//поиск родителя
+	Iterator findParent(Element* child, bool& direction) {
+		return (this->root->pLeft== this->root->pRight)? nullptr:findParent(child->data, child, this->root, direction);
+	}
+	Iterator findParent(T data, Element* child, Element* root,bool& direction) {
+		if (root->pLeft == child) {
+			direction = true;
+			return root;
+		}
+		else if (root->pRight == child) {
+			direction = false;
+			return root;
+		}
+		else {
+			return (data < root->data) ? findParent(data, child, root->pLeft, direction) :
+				findParent(data, child, root->pRight,direction);
+		}
+	}
+	void erase(Element* root) {
+		if (root) {
+			bool direction;
+			Element *temp, *parent;
+			if (root->pRight) {
+				temp = find(minValue(root->pRight));
+				parent = findParent(temp, direction);
+				root->data = temp->data;
+				erase(temp);
+			}
+			else if (root->pLeft) {
+				temp = find(maxValue(root->pLeft));
+				parent = findParent(temp, direction);
+				root->data = temp->data;
+				erase(temp);
+			}
+			else {
+				parent = findParent(root, direction);
+				delete root;
+				if (parent) {
+					(direction) ? parent->pLeft = nullptr : parent->pRight = nullptr;
+				}
+				else {
+					this->root = nullptr;
+				}
+			}
+		}
+	}
 	void print(Element* root) {
 		if (root) {
 			print(root->pLeft);
@@ -64,36 +123,86 @@ class Tree {
 		}
 	}
 public:
+	
+	class Iterator {
+		Element* cur;
+		Element* root;
+	public:
+		Iterator() :cur(nullptr),root(nullptr){}
+		Iterator(Element* cur) :cur(cur) {}
+		//оператор разыменования
+		T& operator*() {
+			return cur->data;
+		}
+		bool operator==(const Iterator& other) const {
+			return this->cur == other.cur;
+		}
+		bool operator!=(const Iterator& other) const {
+			return !operator==(other);
+		}
+		//оператор приведения типов
+		operator Element* () {
+			return this->cur;
+		}
+	};
+	//поиск по значению
+	Iterator find(T data) {
+		return find(data,this->root);
+	}
+	//удаление по значению
+	void erase(T data){
+		Element* del = find(data);
+		erase(del);
+	}
+
 	Tree():root(nullptr){
 	}
 
 	Tree(const Tree& other):Tree() {
 			copy(other.root);
 	}
-	void insert(int data) {
-		insert(data, this->root);
+
+	Tree(const initializer_list<T> il) : Tree() {
+		for (auto el : il) {
+			insert(el);
+		}
 	}
+
+	Tree& operator=(const Tree& other) {
+		if (this != &other) {
+			this->clear();
+			copy(other.root);
+		}
+		return *this;
+	}
+
 	~Tree()	{
 		clear(this->root);
 	}
+	void insert(T data) {
+		insert(data, this->root);
+	}
+
 	void clear() {
 		clear(this->root);
 	}
 	Element* getRoot() {
 		return this->root;
 	}
-	int minValue() {
+	T minValue() {
 		return(root) ? minValue(this->root) : 0;
 	}
-	int maxValue() {
+	T maxValue() {
 		return(root) ? maxValue(this->root) : 0;
 	}
 	int size() {
 		return size(this->root);
 	}
+	//sum только для числовых типов оставим int
 	int sum() {
 		return sum(this->root);
 	}
+	//avg только для числовых типов оставим float
 	float avg() {
 		return (this->root)?(float)sum() / size():0;
 	}
@@ -107,19 +216,30 @@ int main() {
 	system("chcp 1251 > nul");
 	int n;
 
-	Tree t;
+	Tree<int> t;
 
 	cout << "Введите кол-во элементов: "; cin >> n;
 	for (int i = 0; t.size() < n; ++i) {
 		t.insert(rand() % 100);
 	}
 	t.print();
-	Tree t1(t);
+	Tree<int> t1 = t;
+	cout << "t1.erase(67) \n";
+	t1.erase(67);
 	t1.print();
-	cout << "минимальное значение в дереве: "<<t.minValue()<<endl;
-	cout << "максимальное значение в дереве: " << t.maxValue() << endl;
-	cout << "количество элементов дерева: " << t.size() << endl;
-	cout << "Сумма элементов дерева: " << t.sum() << endl;
-	cout << "Среднее значение элементов дерева: " << t.avg() << endl;
+	cout << "минимальное значение в дереве: "<<t1.minValue()<<endl;
+	cout << "максимальное значение в дереве: " << t1.maxValue() << endl;
+	cout << "количество элементов дерева: " << t1.size() << endl;
+	cout << "Сумма элементов дерева: " << t1.sum() << endl;
+	cout << "Среднее значение элементов дерева: " << t1.avg() << endl;
+	
+	cout << "\nТип данных string: " << endl;
+	Tree<string> strTree({ "Hello","World","!" });
+	strTree.print();
+
+	cout << "strTree.erase(\"!\"); \n";
+	strTree.erase("!");
+	strTree.print();
+
 	return 0;
 }
